3e5205e7d12ce2446e06f9f8ee3d0412b884aa37
takikoo <takiko@protonmail.com>
2024-01-28 01:40:49 +0100

    First draft

---
+++ config/kyria.h
@@ -0,0 +1,36 @@
+// clang-format off
+
+// LAYERS
+#define QWERTY 0
+#define LOWER 1
+#define RAISE 2
+#define NAV 3
+#define NUM 4
+#define FUNC 5
+#define ADJUST 6
+
+// CUSTOM KEY CODES
+#define XXX &none
+#define ___ &trans
+
+#define YANK   &kp LC(INS)
+#define PASTE  &kp LS(INS)
+#define KUT    &kp LC(X)
+#define SEL_A  &kp LC(A)
+
+#define C_ESC  &mt LCTRL ESC
+#define BS_LOW &lt LOWER BSPC
+#define ENT_RS &lt RAISE ENTER
+#define SPCNAV &lt NAV SPACE
+#define TABNUM &lt NUM TAB
+#define TABNAV &lt NAV TAB
+#define TABFNC &lt FUNC TAB
+
+#define CLOSE  &kp LA(F4)       // ALT + F4
+#define CAD    &kp LC(LA(DEL))  // CTRL + ALT + DEL
+
+#define MEH      LC(LS(LALT))
+#define TAB_PREV &kp LC(PG_UP)
+#define TAB_NEXT &kp LC(PG_DN)
+
+// clang-format on
